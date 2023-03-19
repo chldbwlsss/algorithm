53ff2f075733c683adcc23fe205791960b806e7d
youjin <developer_choi@naver.com>
2023-03-19 17:43:17 +0900

    [백준] 1316

---
+++ boj/implementation/1316.py
@@ -0,0 +1,13 @@
+### 그룹 단어 체커 ###
+n = int(input())
+cnt = n
+
+for i in range(n):
+    word = input()
+    for j in range(len(word) - 1):
+        if word[j] == word[j + 1]:
+            continue
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
