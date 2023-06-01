98fc6d0bb4c5be4fed258a5ebf659c94a1f3ca79
youjin <developer_choi@naver.com>
2023-06-02 02:28:31 +0900

    [백준] 15649

---
+++ boj/bt/15649.py
@@ -0,0 +1,26 @@
+##N과 M (1)
+import copy
+
+n, m = map(int, input().split())
+
+pool = [x for x in range(1, n+1)]
+result = []
+small = []
+
+def bt(small, depth):
+    if depth == m: #종료조건
+        result.append(copy.deepcopy(small))
+    else:
+        for x in pool:
+            if x not in small:
+                small.append(x)
+                bt(small, depth+1)
+                small.pop()
+
+bt(small, 0)
+
+for x in result:
+    for y in x:
+        print(y, end=" ")
+    print()
+
